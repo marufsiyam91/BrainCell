ad1c2638146c734a7485e341362a1f286eba75c3
marufsiyam91 <marufsiyam9123@gmail.com>
2024-01-16 11:32:15 +0600

    real-lige-project-started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AddTask_Form from './Components/AddTask_Form/AddTask_Form'
+
+function App() {
+
+  return (
+    <>
+        <AddTask_Form/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/AddTask_Form/AddTask_Form.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import styles from "./AddTask_Form.module.css";
+
+const AddTask_Form = () => {
+  const [taskData, setTaskData] = useState({
+    title: "",
+    links: "",
+    description: "",
+    images: "",
+  });
+
+  const [storedData, setStoredData] = useState(null)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData({
+      ...taskData,
+      [name]: value,
+    });
+    console.log(taskData);
+  };
+
+
+  const handleSubmit = (e) => {
+     e.preventDefault()
+     setStoredData({...taskData})
+     console.log(storedData)
+  }
+
+  return (
+    <div onSubmit={handleSubmit} className={styles.addtask_form_wrapper}>
+      <div className={styles.addtask_form_container}>
+        <form className={styles.addtask_form}>
+          <h2>
+            <i>Add task from your brain</i>
+          </h2>
+          <label>
+            Title
+            <input
+              onChange={handleChange}
+              value={taskData.title}
+              type="text"
+              name="title"
+              placeholder="Add Title"
+            />
+          </label>
+
+          <label>
+            Links
+            <input
+              onChange={handleChange}
+              value={taskData.links}
+              type="text"
+              name="links"
+              placeholder="Add Links"
+            />
+          </label>
+
+          <label>
+            Choose image
+            <input
+              onChange={handleChange}
+              value={taskData.images}
+              className={styles.addtask_image}
+              name="images"
+              type="file"
+              placeholder="Add Images"
+            />
+          </label>
+
+          <label>
+            Description
+            <textarea
+              onChange={handleChange}
+              value={taskData.description}
+              name="description"
+              id=""
+              cols="85"
+              rows="10"
+            ></textarea>
+          </label>
+
+          <button className={styles.task_btn}>Add Task</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddTask_Form;
